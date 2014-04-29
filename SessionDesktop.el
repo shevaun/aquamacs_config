43c6ecedb0fa8189103d4902d78557e9849a7255
Shevaun Coker <shevaunc@ibsglobalweb.com>
2014-04-29 17:08:12 +1000

    SC add ack-and-a-half

--- SessionDesktop.el
+++ SessionDesktop.el
@@ -2,40 +2,106 @@
 ;; --------------------------------------------------------------------------
 ;; Desktop File for Emacs
 ;; --------------------------------------------------------------------------
-;; Created Tue Feb 11 12:09:56 2014
+;; Created Wed Apr 23 10:02:51 2014
 ;; Desktop file format version 206
 ;; Emacs version 24.3.50.2
 (setq revive:frame-configuration-to-restore
   '(progn 
-     (revive:restore-frame '((tool-bar-position . top) (explicit-name) (icon-name) (top . 22) (left . 1282) (unsplittable) (width . 178) (height . 99) (fringe) (modeline . t) (border-color . "#232323") (color-theme-name . color-theme-railscasts) (mouse-color . "sienna1") (environment) (visibility . t) (cursor-color . "red") (background-mode . dark) (horizontal-scroll-bars . t) (fullscreen) (alpha) (scroll-bar-width . 15) (cursor-type . box) (auto-lower) (auto-raise) (icon-type) (title) (buffer-predicate) (tool-bar-lines . 0) (menu-bar-lines . 1) (right-fringe . 11) (left-fringe . 3) (line-spacing) (background-color . "#232323") (foreground-color . "#E6E1DC") (vertical-scroll-bars . right) (internal-border-width . 0) (border-width . 0) (font . "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1") (fontsize . 0)) '(178 99 ((0 0 183 97)) (("~/Library/Preferences/Aquamacs Emacs/Preferences.el" "Preferences.el" 1376 1376 ((tab "*scratch*" "/Users/shevaunc/Library/Application Support/Aquamacs Emacs/scratch buffer") (tab "Preferences.el" "/Users/shevaunc/Library/Preferences/Aquamacs Emacs/Preferences.el"))))))))
+     (revive:restore-frame '((tool-bar-position . top) (explicit-name) (icon-name) (top . 22) (left . 1281) (unsplittable) (width . 156) (height . 82) (modeline . t) (fringe) (border-color . "#232323") (color-theme-name . color-theme-railscasts) (mouse-color . "sienna1") (environment) (visibility . t) (cursor-color . "red") (background-mode . dark) (horizontal-scroll-bars . t) (fullscreen) (alpha) (scroll-bar-width . 15) (cursor-type . box) (auto-lower) (auto-raise) (icon-type) (title) (buffer-predicate) (tool-bar-lines . 0) (menu-bar-lines . 1) (right-fringe . 12) (left-fringe . 4) (line-spacing) (background-color . "#232323") (foreground-color . "#E6E1DC") (vertical-scroll-bars . right) (internal-border-width . 0) (border-width . 0) (font . "-apple-Monaco-medium-normal-normal-*-13-*-*-*-m-0-iso10646-1") (fontsize . 0)) '(156 82 ((0 0 160 40) (0 40 160 81)) (("~/src/ibsa/fm_commissions/app/views/partner_commission_types/_form.html.erb" "_form.html.erb" 94 1 ((tab "*scratch*" "/Users/shevaunc/Library/Application Support/Aquamacs Emacs/scratch buffer") (tab "schema.rb" "/Users/shevaunc/src/ibsa/fm_commissions/db/schema.rb") (tab "_form.html.erb" "/Users/shevaunc/src/ibsa/fm_commissions/app/views/partner_commission_types/_form.html.erb") (tab "finance_manager_helper.rb" "/Users/shevaunc/src/ibsa/fm_commissions/lib/helpers/finance_manager_helper.rb") (tab "command_button.js.coffee" "/Users/shevaunc/src/ibsa/fm_commissions/app/assets/javascripts/command_button.js.coffee") (tab "commission_adjustment_handler.js.coffee" "/Users/shevaunc/src/ibsa/fm_commissions/app/assets/javascripts/commission_adjustment_handler.js.coffee") (tab "application.js.coffee" "/Users/shevaunc/src/ibsa/fm_commissions/app/assets/javascripts/application.js.coffee"))) ("~/src/ibsa/fm_commissions/app/views/partner_commission_types/_form.html.erb" "_form.html.erb" 3635 2712 ((tab "date_picker_factory.js.coffee" "/Users/shevaunc/src/ibsa/fm_commissions/app/assets/javascripts/date_picker_factory.js.coffee") (tab "commission_adjustment_handler.js.coffee" "/Users/shevaunc/src/ibsa/fm_commissions/app/assets/javascripts/commission_adjustment_handler.js.coffee") (tab "command_button.js.coffee" "/Users/shevaunc/src/ibsa/fm_commissions/app/assets/javascripts/command_button.js.coffee") (tab "finance_manager_helper.rb" "/Users/shevaunc/src/ibsa/fm_commissions/lib/helpers/finance_manager_helper.rb") (tab "_form.html.erb" "/Users/shevaunc/src/ibsa/fm_commissions/app/views/partner_commission_types/_form.html.erb"))))))))
 
 ;; Global section:
-(setq desktop-saved-frameset [frameset 1 (21241 30820 263006 0) (desktop . "206") "shevaunc@Shevauns-MacBook-Air-2.local" nil nil ((((font-backend ns) (fontsize . 0) (font . "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 0) (vertical-scroll-bars . right) (foreground-color . "#E6E1DC") (background-color . "#232323") (line-spacing) (left-fringe . 3) (right-fringe . 11) (menu-bar-lines . 1) (tool-bar-lines . 0) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 15) (alpha) (fullscreen) (horizontal-scroll-bars . t) (display-type . color) (background-mode . dark) (cursor-color . "red") (visibility . t) (environment) (mouse-color . "sienna1") (color-theme-name . color-theme-railscasts) (border-color . "#232323") (modeline . t) (fringe) (frameset--id . "97EF-E600-7307-4B74") (frameset--mini t . t) (height . 99) (width . 178) (minibuffer . t) (unsplittable) (left . 1282) (top . 22) (icon-name) (display . "Shevauns-MacBook-Air-2.local") (explicit-name) (tool-bar-position . top)) ((min-height . 4) (min-width . 10) (min-height-ignore . 3) (min-width-ignore . 8) (min-height-safe . 1) (min-width-safe . 2)) leaf (total-height . 97) (total-width . 183) (normal-height . 1.0) (normal-width . 1.0) (buffer "Preferences.el" (selected . t) (hscroll . 0) (fringes 3 11 nil) (margins nil) (scroll-bars 15 3 t nil) (vscroll . 0) (dedicated) (point . 1376) (start . 1376))))])
+(setq desktop-saved-frameset [frameset 1 (21335 811 422856 0) (desktop . "206") "shevaunc@ShevaunAir" nil nil ((((font-backend ns) (fontsize . 0) (font . "-apple-Monaco-medium-normal-normal-*-13-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 0) (vertical-scroll-bars . right) (foreground-color . "#E6E1DC") (background-color . "#232323") (line-spacing) (left-fringe . 4) (right-fringe . 12) (menu-bar-lines . 1) (tool-bar-lines . 0) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 15) (alpha) (fullscreen) (horizontal-scroll-bars . t) (display-type . color) (background-mode . dark) (cursor-color . "red") (visibility . t) (environment) (mouse-color . "sienna1") (color-theme-name . color-theme-railscasts) (border-color . "#232323") (fringe) (modeline . t) (frameset--id . "7DF8-40DB-1CAE-BF83") (frameset--mini t . t) (height . 82) (width . 156) (minibuffer . t) (unsplittable) (left . 1281) (top . 22) (icon-name) (display . "ShevaunAir") (explicit-name) (tool-bar-position . top)) ((min-height . 8) (min-width . 10) (min-height-ignore . 6) (min-width-ignore . 7) (min-height-safe . 2) (min-width-safe . 2)) vc (total-height . 81) (total-width . 160) (normal-height . 1.0) (normal-width . 1.0) (combination-limit) (leaf (total-height . 40) (total-width . 160) (normal-height . 0.5) (normal-width . 1.0) (buffer "_form.html.erb" (selected . t) (hscroll . 0) (fringes 4 12 nil) (margins 3) (scroll-bars 15 2 t nil) (vscroll . 0) (dedicated) (point . 94) (start . 1))) (leaf (last . t) (total-height . 41) (total-width . 160) (normal-height . 0.5) (normal-width . 1.0) (buffer "_form.html.erb" (selected) (hscroll . 0) (fringes 4 12 nil) (margins 3) (scroll-bars 15 2 t nil) (vscroll . 0) (dedicated) (point . 3635) (start . 2712)))))])
 (setq desktop-missing-file-warning nil)
 (setq tags-file-name nil)
 (setq tags-table-list nil)
-(setq search-ring '("haml" "haml-mode" "haml-mo" "haml-mode" "sass-mode" "sass" "fm_auditor" "customis" "logged" "change" "history" "change_desc" "calculation_script" "contract_setup" "888" "commissionadjustmentservice"))
+(setq search-ring '("add_transaction" "@interest_account" "terminat" "partner_commission_types" "valid_with_partner_commission_types" "proportion" "java" "execute_browser_specs" "repayments" "calculate_repayments" "pending" "terminate!" "save_and_redirect" "RAILS_ENV" "ci_gem_dir" "back"))
 (setq regexp-search-ring '("[A-Z]" "[A-Z]\\{4\\} " "[A-Z]\\{4\\}" "[A-Z]" "([A-Z])\\{1\\}"))
-(setq register-alist nil)
-(setq file-name-history '("~/.ido.last" "~/Library/Preferences/Aquamacs Emacs/Preferences.el" "~/.emacs.bmk" "~/.ido.last" "~/Library/Preferences/Aquamacs Emacs/Preferences.el" "~/.emacs.bmk" "~/.ido.last" "~/Library/Preferences/Aquamacs Emacs/Preferences.el" "/Users/shevaunc/Library/Application Support/Aquamacs Emacs/haml-mode.el" "~/Library/Application Support/Aquamacs Emacs/elpa/haml-mode-3.1.5/haml-mode-autoloads.el" "~/Library/Application Support/Aquamacs Emacs/elpa/archives/gnu/archive-contents" "~/Library/Application Support/Aquamacs Emacs/elpa/archives/marmalade/archive-contents" "~/Library/Application Support/Aquamacs Emacs/elpa/archives/elpa/archive-contents" "~/Library/Application Support/Aquamacs Emacs/elpa/archives/gnu/archive-contents" "~/Library/Application Support/Aquamacs Emacs/elpa/archives/marmalade/archive-contents" "~/Library/Application Support/Aquamacs Emacs/elpa/archives/elpa/archive-contents" "/Users/shevaunc/Library/Application Support/Aquamacs Emacs/sass-mode.el" "/Users/shevaunc/src/ibsa/fm_admin/app/assets/stylesheets/base_rate.css.scss" "~/src/ibsa/fm_admin/db/schema.rb" "~/Library/Application Support/Aquamacs Emacs/elpa/sass-mode-3.0.16/sass-mode-autoloads.el" "~/Library/Application Support/Aquamacs Emacs/elpa/haml-mode-3.1.5/haml-mode-autoloads.el" "~/Library/Application Support/Aquamacs Emacs/elpa/archives/gnu/archive-contents" "~/Library/Application Support/Aquamacs Emacs/elpa/archives/marmalade/archive-contents" "~/Library/Application Support/Aquamacs Emacs/elpa/archives/elpa/archive-contents" "~/Library/Application Support/Aquamacs Emacs/elpa/archives/gnu/archive-contents" "~/Library/Application Support/Aquamacs Emacs/elpa/archives/marmalade/archive-contents" "~/Library/Application Support/Aquamacs Emacs/elpa/archives/elpa/archive-contents" "~/Library/Preferences/Aquamacs Emacs/Preferences.el" "~/.emacs.bmk" "~/Library/Preferences/Aquamacs Emacs/Preferences.el" "~/.emacs.bmk" "~/.ido.last" "~/Library/Preferences/Aquamacs Emacs/Preferences.el" "~/Library/Application Support/Aquamacs Emacs/elpa/archives/gnu/archive-contents" "/Users/shevaunc/Library/Application Support/Aquamacs Emacs/sass-mode.el" "/Users/shevaunc/src/ibsa/fm_admin/app/assets/stylesheets/base_rate.css.scss" "~/src/ibsa/fm_admin/db/schema.rb" "~/Library/Preferences/Aquamacs Emacs/Preferences.el"))
+(setq register-alist '((48 . "<%= form_for @partner, remote:true, url: partner_commission_type_path(@partner.id) do |f| %>\n  <div class=\"fields\">\n    <div class=\"panel\">\n      <div class=\"subheading\">\n        <span>Details</span>\n      </div>\n      <div class=\"left_column\">\n        <%= show_field('Name', @partner.name) %>\n      </div>\n      <div class=\"right_column\">\n        <%= show_field('Type', @partner.underlying_entity_type.underscore.titleize) %>\n      </div>\n    </div>\n    <div class=\"panel\">\n      <div class=\"subheading\">\n        <span>Commission Types</span>\n      </div>\n      <% @partner.partner_commission_types.each do |partner_commission_type| %>\n        <%= f.fields_for \"partner_commission_types\".to_sym, partner_commission_type, :index => partner_commission_type.commission_type.id do |p| %>\n          <%= sub_heading partner_commission_type.commission_type.name %>\n          <div class=\"left_column\">\n            <%= form_field_yes_no p, commission_name(partner_commission_type), :applied,  :disabled => !partner_commission_type.editable?  %>\n          </div>\n          <div class=\"right_column\">\n            <div id=\"effective_date_<%=partner_commission_type.commission_type.id%>\">\n              <%= form_field_date(p, 'Effective Date', :formatted_effective_date, :mandatory => true, :disabled => !partner_commission_type.editable?) %>\n            </div>\n            <div id=\"removal_effective_date_<%=partner_commission_type.commission_type.id%>\">\n              <%= form_field_date(p, 'Removal Effective Date', :formatted_removal_effective_date, :mandatory => true, :disabled => !partner_commission_type.editable?|| partner_commission_type.removed?) unless partner_commission_type.new_record? %>\n            </div>\n          </div>\n          <%= javascript_tag do %>\n            <% unless partner_commission_type.removed? || partner_commission_type.pending_removal? %>\n              $(document).ready(function(){$(\"#removal_effective_date_<%=partner_commission_type.commission_type.id%>\").hide()});\n            <% end %>\n            <% if partner_commission_type.editable? %>\n              $(\"#partner_partner_commission_types_<%=partner_commission_type.commission_type.id%>_applied_true\").click(\n                function(){$(\"#removal_effective_date_<%=partner_commission_type.commission_type.id%>\").hide()}\n              );\n              $(\"#partner_partner_commission_types_<%=partner_commission_type.commission_type.id%>_applied_false\").click(\n                function(){$(\"#removal_effective_date_<%=partner_commission_type.commission_type.id%>\").show()}\n              );\n            <% end %>\n            <% if partner_commission_type.new_record? %>\n              <% unless partner_commission_type.applied %>\n                $(document).ready(function(){$(\"#effective_date_<%=partner_commission_type.commission_type.id%>\").hide()});\n              <% end %>\n              $(\"#partner_partner_commission_types_<%=partner_commission_type.commission_type.id%>_applied_true\").click(\n                function(){$(\"#effective_date_<%=partner_commission_type.commission_type.id%>\").show()}\n              );\n              $(\"#partner_partner_commission_types_<%=partner_commission_type.commission_type.id%>_applied_false\").click(\n                function(){$(\"#effective_date_<%=partner_commission_type.commission_type.id%>\").hide()}\n              );\n              new DatePickerFactory()\n            <% end %>\n          <% end %>\n        <% end %>\n      <% end %>\n    </div>\n  </div>\n  <div class=\"action_buttons\">\n    <%= submit_button %>\n    <%= link_to 'Cancel', {:action => :show}, :class => 'submit_button button' %>\n  </div>\n<% end %>\n")))
+(setq file-name-history '("~/.ido.last" "~/src/ibsa/fm_commissions/app/views/partner_commission_types/_form.html.erb" "/Users/shevaunc/src/ibsa/fm_commissions/app/assets/javascripts/application.js.coffee" "/Users/shevaunc/src/ibsa/fm_commissions/app/assets/javascripts/commission_adjustment_handler.js.coffee" "/Users/shevaunc/src/ibsa/fm_commissions/app/assets/javascripts/command_button.js.coffee" "/Users/shevaunc/src/ibsa/fm_commissions/app/assets/javascripts/date_picker_factory.js.coffee" "~/src/ibsa/fm_commissions/lib/helpers/finance_manager_helper.rb" "~/src/ibsa/fm_commissions/db/schema.rb" "~/.emacs.bmk" "~/.ido.last" "/Users/shevaunc/src/ibsa/fm_commissions/app/models/account_transaction.rb" "/Users/shevaunc/src/ibsa/fm_commissions/app/models/interest_capitalization_transaction.rb" "/Users/shevaunc/src/ibsa/fm_commissions/app/views/partner_commission_types/_form.html.erb" "~/src/ibsa/fm_commissions/app/views/partner_commission_types/edit.html.erb" "~/src/ibsa/fm_commissions/app/services/accounting_extract_producer.rb" "/Users/shevaunc/src/ibsa/fm_commissions/app/controllers/accounting_extracts_controller.rb" "/Users/shevaunc/src/ibsa/fm_commissions/app/models/commission_financial_event.rb" "/Users/shevaunc/src/ibsa/fm_commissions/app/background_processes/generate_financial_events.rb" "/Users/shevaunc/src/ibsa/fm_commissions/app/background_processes/get_financiers.rb" "/Users/shevaunc/src/ibsa/fm_commissions/app/models/accounting_extract_line_item.rb" "/Users/shevaunc/src/ibsa/fm_commissions/app/models/accounting/commissions_transaction_type.rb" "/Users/shevaunc/src/ibsa/fm_commissions/spec/models/amortisation_calculator/effective_interest_rate_spec.rb" "~/src/ibsa/fm_commissions/app/models/amortisation_calculator/effective_interest_rate.rb" "~/.rvm/gems/ruby-1.9.3-p545/gems/repayment_calculator-2.1.3/lib/accounting/common/account_module.rb" "~/.rvm/gems/ruby-1.9.3-p545/gems/repayment_calculator-2.1.3/lib/repayment_calculator/repayment_simulator.rb" "/Users/shevaunc/.rvm/gems/ruby-1.9.3-p545/gems/repayment_calculator-2.1.3/lib/repayment_calculator.rb" "~/.rvm/gems/ruby-1.9.3-p545/gems/repayment_calculator-2.1.3/lib/accounting/transaction_type.rb" "/Users/shevaunc/.rvm/gems/ruby-1.9.3-p545/gems/repayment_calculator-2.1.3" "/Users/shevaunc/src/ibsa/fm_commissions/app/models/contract.rb" "~/src/ibsa/fm_commissions/app/models/commission_facility.rb" "~/src/ibsa/fm_commissions/app/models/commission_financial_event.rb"))
 
 ;; Buffer section -- buffers listed in same order as in buffer list:
 (desktop-create-buffer 206
-  "/Users/shevaunc/Library/Preferences/Aquamacs Emacs/Preferences.el"
-  "Preferences.el"
-  'emacs-lisp-mode
-  '(flx-ido-mode)
-  1376
-  '(5048 nil)
+  "/Users/shevaunc/src/ibsa/fm_commissions/app/views/partner_commission_types/_form.html.erb"
+  "_form.html.erb"
+  'rhtml-mode
+  '(abbrev-mode smart-spacing-mode linum-mode flx-ido-mode rails-minor-mode rails-view-minor-mode)
+  94
+  '(3636 nil)
   nil
   nil
-  '((buffer-file-coding-system . prefer-utf-8-unix)))
+  '((buffer-file-coding-system . utf-8-unix)))
+
+(desktop-create-buffer 206
+  "/Users/shevaunc/src/ibsa/fm_commissions/app/assets/javascripts/application.js.coffee"
+  "application.js.coffee"
+  'coffee-mode
+  '(linum-mode flx-ido-mode rails-minor-mode)
+  517
+  '(533 nil)
+  nil
+  nil
+  '((indent-tabs-mode) (buffer-file-coding-system . undecided-unix)))
+
+(desktop-create-buffer 206
+  "/Users/shevaunc/src/ibsa/fm_commissions/app/assets/javascripts/commission_adjustment_handler.js.coffee"
+  "commission_adjustment_handler.js.coffee"
+  'coffee-mode
+  '(linum-mode flx-ido-mode rails-minor-mode)
+  613
+  '(nil nil)
+  nil
+  nil
+  '((indent-tabs-mode) (buffer-file-coding-system . undecided-unix)))
+
+(desktop-create-buffer 206
+  "/Users/shevaunc/src/ibsa/fm_commissions/app/assets/javascripts/date_picker_factory.js.coffee"
+  "date_picker_factory.js.coffee"
+  'coffee-mode
+  '(linum-mode flx-ido-mode rails-minor-mode)
+  530
+  '(374 nil)
+  nil
+  nil
+  '((indent-tabs-mode) (buffer-file-coding-system . undecided-unix)))
+
+(desktop-create-buffer 206
+  "/Users/shevaunc/src/ibsa/fm_commissions/app/assets/javascripts/command_button.js.coffee"
+  "command_button.js.coffee"
+  'coffee-mode
+  '(linum-mode flx-ido-mode rails-minor-mode)
+  193
+  '(nil nil)
+  nil
+  nil
+  '((indent-tabs-mode) (buffer-file-coding-system . undecided-unix)))
+
+(desktop-create-buffer 206
+  "/Users/shevaunc/src/ibsa/fm_commissions/lib/helpers/finance_manager_helper.rb"
+  "finance_manager_helper.rb"
+  'ruby-mode
+  '(linum-mode flx-ido-mode rails-minor-mode rails-lib-minor-mode)
+  13959
+  '(13959 nil)
+  nil
+  nil
+  '((indent-tabs-mode) (buffer-file-coding-system . utf-8-unix)))
+
+(desktop-create-buffer 206
+  "/Users/shevaunc/src/ibsa/fm_commissions/db/schema.rb"
+  "schema.rb"
+  'ruby-mode
+  '(linum-mode flx-ido-mode rails-minor-mode)
+  1
+  '(nil nil)
+  nil
+  nil
+  '((indent-tabs-mode) (buffer-file-coding-system . utf-8-unix)))
 
 (desktop-create-buffer 206
   "/Users/shevaunc/Library/Application Support/Aquamacs Emacs/scratch buffer"
   "*scratch*"
   'text-mode
-  '(smart-spacing-mode flx-ido-mode)
+  '(smart-spacing-mode linum-mode flx-ido-mode)
   1
   '(nil nil)
   nil
